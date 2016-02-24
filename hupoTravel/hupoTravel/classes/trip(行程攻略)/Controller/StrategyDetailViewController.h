1175435f98f1db2f52ee1ab4e7d276ec7ee3822d
DomenicL <3377926196@qq.com>
2016-02-24 22:26:37 +0800

    完整代码

---
+++ hupoTravel/hupoTravel/classes/trip(行程攻略)/Controller/StrategyDetailViewController.h
@@ -0,0 +1,20 @@
+//
+//  StrategyDetailViewController.h
+//  琥珀旅行
+//
+//  Created by 卢育彪 on 16/2/15.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StrategyDetailViewController : UIViewController
+
+//接收单元格代表的城市或国家的ID
+@property (nonatomic, strong) NSNumber *placeID;
+@property (nonatomic, copy) NSString *name;
+
+//区分国内外
+@property (nonatomic, assign) BOOL isContryOut;
+
+@end
